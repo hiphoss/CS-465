3ea7b7a28ab2d0d780922da7cbe326b42885f57f
hiphoss <hiphoss@yahoo.com>
2026-02-21 11:32:34 -0700

    Final project - Restored MVC routing, connected MongoDB, updated layout and views

--- app.js
+++ app.js
@@ -1,51 +1,58 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var mongoose = require('mongoose');
-require('./models/trips');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var tripsRouter = require('./routes/trips');
-var cors = require('cors');
+var mongoose = require('mongoose');
 
+// CONNECT TO MONGODB (Updated for modern Mongoose)
+mongoose.connect('mongodb://127.0.0.1:27017/travlr');
+
+mongoose.connection.on('connected', function () {
+  console.log('MongoDB connected');
+});
+
+mongoose.connection.on('error', function (err) {
+  console.log('MongoDB connection error:', err);
+});
+
+// REGISTER MODELS FIRST
+require('./models/trips');
+require('./models/user');
+
+// THEN REQUIRE ROUTES
 var indexRouter = require('./routes/index');
+var tripsRouter = require('./routes/trips');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/travlr');
-
-mongoose.connection.on('connected', () => {
-  console.log('MongoDB connected');
-});
-require('./models/trips');
-
-// view engine setup
+// VIEW ENGINE SETUP
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// MIDDLEWARE
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ROUTES
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api', tripsRouter);
+app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// CATCH 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// ERROR HANDLER
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var Trip = mongoose.model('Trip');
+
+/* HOME */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Travlr Getaways' });
+});
+
+/* TRAVEL - dynamic */
+router.get('/travel', async function(req, res, next) {
+  try {
+    const trips = await Trip.find({}).exec();
+    res.render('travel', {
+      title: 'Travel',
+      trips: trips
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+/* NEWS */
+router.get('/news', function(req, res) {
+  res.render('news', { title: 'News' });
+});
+
+/* RESERVATIONS */
+router.get('/reservations', function(req, res) {
+  res.render('reservations', { title: 'Reservations' });
+});
+
+/* ADMIN */
+router.get('/admin', function(req, res) {
+  res.render('admin', { title: 'Admin' });
+});
+
+/* LOGIN */
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Login' });
 });
 
 module.exports = router;
